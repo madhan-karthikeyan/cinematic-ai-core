d2a2dff2f708314d30399b64fe910a859a4ff7f9
madhan-karthikeyan <madhankrthik@gmail.com>
2026-05-15 19:12:57 +0530

    added files

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import "./lib/error-capture";
 
 import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
-import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
 
-export default createStartHandler(defaultStreamHandler);
+const handler = createStartHandler(defaultStreamHandler);
+
+export default { fetch: handler };
